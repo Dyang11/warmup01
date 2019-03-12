e2f9c990e277ac7ec87db77ed964b58287363c6b
Daniel Yang <d_yang11@berkeley.edu>
2019-03-11 17:12:13 -0700

    downloading court image:

---
+++ code/make-shot-chart-scripts.R
@@ -0,0 +1,7 @@
+#Title: Make Shots Charts Script
+#Description: Creating shot chart visualizations for each player's shot attempts
+#Inputs: Data frames of the players' shots
+#Outputs: A pdf overview of each player's shots over a NBA basketball court
+
+library(jpeg)
+library(grid)
